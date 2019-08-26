ba218bc84a9adcd990acd792a9f6606a534e8033
Ahmed Nasr <ahmed@ahmednasr.me>
2019-08-26 10:06:56 -0400

    Added scan range

--- portscan/advscan.py
+++ portscan/advscan.py
@@ -3,6 +3,7 @@
 from socket import *
 from threading import *
 import optparse
+import re
 
 def connScan(host,port):
 	try:
@@ -25,14 +26,20 @@ def port_scan(Host,ports):
 		t = Thread(target = connScan, args = (Host, int(port)))
 		t.start()
 def main():
-	parser = optparse.OptionParser('Usage: '+'-H <Host> -P <Ports>')
+	parser = optparse.OptionParser('Usage: '+'-H <Host> -P <Ports> Specific ports seperated by comma, or as range ex: 82-443')
 	parser.add_option('-H', dest='host', type='string', help='Target Host')
 	parser.add_option('-P', dest='ports', type='string', help='Target PortS seperated by comma')
 	(options, args) = parser.parse_args()
 	host = options.host
 	ports = str(options.ports).split(',')
-	if (host == None) | (ports[0] == None):
+	if (host == 'None') | (ports[0] == 'None'):
 		print parser.usage
 		exit()
+	elif (len(ports) == 1) and (re.search('[0-9*]-[0-9*]',ports[0]) is not None):
+		print 'Scan range:' + ports[0]
+		ports = ports[0].split('-')
+		ports = range(int(ports[0]),int(ports[1])+1)
+	print ports
 	port_scan(host,ports)
-main()
+if __name__ == '__main__':
+	main()
